69c81753c03d6d5dd372791d5676b31a63227599
rohit1421 <112781387+rohit1421@users.noreply.github.com>
2024-02-10 18:21:22 +0530

    Add files via upload

---
+++ assets/js/email.js
@@ -0,0 +1,14 @@
+function sendEmail(){
+    // Email.send({
+    //     Host : "smtp.gmail.com",
+    //     Username : "mrohith142001@gmail.com.com",
+    //     Password : "",
+    //     To : 'mrohith142001@gmail.com.com',
+    //     From : document.getElementById("email").value,
+    //     Subject : "Website Mail",
+    //     Body : "this is the body"
+    // }).then(
+    //   message => alert(message)
+    // );
+    // console.log(document.getElementById("email").value);
+}
